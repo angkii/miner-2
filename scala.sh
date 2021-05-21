273818cb6a68f3b8c5f449266e1259c7a93baee0
angkii <83603708+angkii@users.noreply.github.com>
2021-05-22 01:40:59 +0700

    Create scala.sh

---
+++ scala.sh
@@ -0,0 +1,3 @@
+# Scala XLA Coin
+sudo apt update
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o ca.scala.herominers.com:10131 -u Siz7KzWmiNWSRTjZBAt5qqWC5PPYLVGvQdxfDPsyMXpUMkRuzwKNLEbfevN4Eoh76gTvxPeszEj5R7GmJMnkHDD8Ms7Peb4xquq1UeZmhjZHY -p gp1 -a panthera -k -t4
